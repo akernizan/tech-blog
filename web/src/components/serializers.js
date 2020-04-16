b2e24cde6a4d3c97193291f0d40639ba04a73a5f
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-04-16 17:07:53 -0400

    added code block

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Figure from './Figure'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 const serializers = {
   types: {
     authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
+    mainImage: Figure,
+    code: ({ node = {} }) => {
+      const [code, language ] = node;
+
+      if(!code){
+        return null;
+      }
+      <SyntaxHighlighter language={language || "text"}>
+        {code}
+      </SyntaxHighlighter>
+    }
   }
 }
 
