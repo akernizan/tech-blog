1ba77d47e01fa3ad7bb01123db0b43288c2b1980
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-05-20 17:43:06 -0400

    added GA

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,6 +9,17 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId:process.env.GOOGLE_ANALYTICS_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
     {
